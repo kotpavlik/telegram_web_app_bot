bdf8712d5c531234aa2630560b004877cabd4ea6
kotpavlik <kotpavlik88@gmail.com>
2023-09-27 02:12:15 +0300

    fix uuid for vercel

--- PinkPunkService/fileService.js
+++ PinkPunkService/fileService.js
@@ -1,11 +1,13 @@
-import * as uuid from 'uuid';
+import {
+    v4 as uuidv4
+} from 'uuid';
 import * as path from 'path';
 
 
 class FileService {
     saveFile(file) {
         try {
-            const file_name = uuid.v4() + '.jpg';
+            const file_name = uuidv4() + '.jpg';
             const filePath = path.resolve('static', file_name)
             file.mv(filePath);
             return file_name
